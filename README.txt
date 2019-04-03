2e52bfe9ff80d450eba9843a40add7d58e2e5dcf
¨Yunet¨ <yunet.lruiz94@gmail.com>
2019-04-03 17:45:01 -0300

    first commit

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/yunet-luis-ruiz/pen/RddMZa.
+A Pen created at CodePen.io. You can find this one at https://codepen.io/yunet-luis-ruiz/pen/exovGK.
 
  
--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
-function {
-  
-}
+var btn = document.getElementById("navBtn");
+var ul = document.getElementById("menu");
+var divNav = document.getElementById("nav-right");
+var hero = document.getElementById("hero");
+
+btn.addEventListener("click", function(){
+   ul.classList.toggle("active");
+   // hero.style.marginTop=180+'px';
+});
